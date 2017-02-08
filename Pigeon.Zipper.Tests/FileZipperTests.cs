d6e0e5484ff68cd2c4a8710c557f66a57ab9a0b2
deeja <deeja@users.noreply.github.com>
2017-02-08 11:07:39 +0000

    Testing file zipper

---
+++ Pigeon.Zipper.Tests/FileZipperTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigeon.Zipper.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FileZipperTests
+    {
+
+        [Test]
+        public void ZipFiles_NoFiles_NoZip()
+        {   
+            FileZipper zipper = new FileZipper();
+            ZipResult zipResult = zipper.ZipFiles(Enumerable.Empty<string>());
+            Assert.IsNull(zipResult);
+        }
+
+        [Test]
+        public void ZipFiles_OneFile_ZipResult()
+        {   
+            FileZipper zipper = new FileZipper();
+            ZipResult zipResult = zipper.ZipFiles(new[] { "testfile.zip"});
+            Assert.IsNotNull(zipResult);
+        }
+    }
+
+
+
+}
---
+++ Pigeon.Zipper/FileZipper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pigeon.Zipper
+{
+    public class FileZipper
+    {
+        public ZipResult ZipFiles(IEnumerable<string> fileList)
+        {
+            return null;
+        }
+    }
+
+    public class ZipResult
+    {
+    }
+}
