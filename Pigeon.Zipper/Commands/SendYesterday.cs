03e0e81dd5d534ccf7c07a50e3c0ca742bf15b21
deeja <deeja@users.noreply.github.com>
2017-02-10 12:09:32 +0000

    Adding commands for yesterday and today

---
+++ Pigeon.Zipper/Commands/SendYesterday.cs
@@ -0,0 +1,22 @@
+﻿namespace Pigeon.Zipper.Commands
+{
+    using System;
+
+    using Pipelines;
+
+    using Sitecore.Pipelines;
+    using Sitecore.Shell.Framework.Commands;
+
+    public class SendYesterday: Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            var pipelineArgs = new PigeonPipelineArgs()
+                                   {
+                                       StartDateTime = DateTime.Today - TimeSpan.FromDays(1),
+                                       EndDateTime = DateTime.Today
+                                   };
+            CorePipeline.Run("pigeon.Execute", pipelineArgs);
+        }
+    }
+}
