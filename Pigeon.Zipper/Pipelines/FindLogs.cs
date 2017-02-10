b2e4e1e62ceec3842c0c8c0e90e4fd58d2294d42
deeja <deeja@users.noreply.github.com>
2017-02-10 11:12:57 +0000

    Add filedirectory null check

--- Pigeon.Zipper/Pipelines/FindLogs.cs
+++ Pigeon.Zipper/Pipelines/FindLogs.cs
@@ -8,6 +8,7 @@
         public override void Process(PigeonPipelineArgs args)
         {
             Assert.IsNotNull(args, "args != null");
+            Assert.IsNotNull(args.FileDirectory, "args.FileDirectory != null");
             FileFinder finder = new FileFinder(args.FileDirectory);
             args.FoundFiles = finder.FindLogFiles(args.StartDateTime, args.EndDateTime);
         }
